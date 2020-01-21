d6b817d6c5ba180d4091186756a9e664911e95d7
luc-d-miller <luc.d.miller@gmail.com>
2020-01-21 12:44:14 -0600

    Initial Commit

---
+++ Nursing Inventory Companion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nursing Inventory Companion
+//
+//  Created by Lucas Miller on 1/21/20.
+//  Copyright © 2020 Lucas Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
